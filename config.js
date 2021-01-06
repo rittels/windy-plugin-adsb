89d0d70095a0601e7b895c05542a15fdeee9f1ba
rittels <44720431+rittels@users.noreply.github.com>
2021-01-06 20:41:12 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -6,7 +6,9 @@ module.exports = {
     dependencies: [
         'https://unpkg.com/windyplugin-module-pickertools@0.1.0/dist/pickerTools.js',
         'https://unpkg.com/windyplugin-module-infobox@0.0.8/dist/infobox.js',
-        'https://unpkg.com/windyplugin-module-airspaces@0.0.34/dist/airspaces.js'
+        'https://unpkg.com/windyplugin-module-airspaces@0.0.34/dist/airspaces.js',
+        'https://unpkg.com/geobuf@3.0.2/dist/geobuf.js',
+        'https://unpkg.com/pbf@3.0.5/dist/pbf.js'
     ],
 	exclusive: 'lhpane',
 }
